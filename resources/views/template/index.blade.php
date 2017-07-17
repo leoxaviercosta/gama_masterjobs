5a6b25f185d341c10fcbc2a2073e39acf8097eb6
Leonardo Costa Carvalho <contato@techprise.com.br>
2017-07-17 13:38:10 -0300

    google analyctis

--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -21,6 +21,16 @@
 
     <!-- Favicon -->
     <link rel="icon" href="assets/img/icon-green.ico">
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-15631848-30', 'auto');
+      ga('send', 'pageview');
+
+    </script>
 </head>
 
 <body>
