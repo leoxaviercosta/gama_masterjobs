bfd7ba56186ca94b8be580a61a525137e9b31db3
Leonardo Costa Carvalho <contato@techprise.com.br>
2017-07-17 13:33:18 -0300

    google analyctis

--- resources/views/template/index.blade.php
+++ resources/views/template/index.blade.php
@@ -24,16 +24,6 @@
 </head>
 
 <body>
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-      ga('create', 'UA-102671259-1', 'auto');
-      ga('send', 'pageview');
-
-    </script>
     <header>
         <div class="menu">
             <div class="container">
@@ -322,6 +312,16 @@
     <script type="text/javascript" src="libs/jquery/jquery.countdown.min.js"></script>
     <script type="text/javascript" src="libs/bootstrap/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="assets/js/main.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-102671259-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
 </body>
 
 </html>
