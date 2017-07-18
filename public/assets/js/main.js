b3a340c9e513db559dbe06f22d9f345ef891d73f
Roberth Godoy <roberth.godoy@yahoo.com>
2017-07-18 01:01:42 -0300

    add fluid width youtube

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -82,4 +82,40 @@ function openNav() {
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-} 
+}
+
+// Find all YouTube videos
+var $allVideos = $("iframe[src^='//www.youtube.com']"),
+
+    // The element that is fluid width
+    $fluidEl = $(".mj-blog-sub-content");
+
+// Figure out and save aspect ratio for each video
+$allVideos.each(function () {
+
+    $(this)
+        .data('aspectRatio', this.height / this.width)
+
+        // and remove the hard coded width/height
+        .removeAttr('height')
+        .removeAttr('width');
+
+});
+
+// When the window is resized
+$(window).resize(function () {
+
+    var newWidth = $fluidEl.width();
+
+    // Resize all videos according to their own aspect ratio
+    $allVideos.each(function () {
+
+        var $el = $(this);
+        $el
+            .width(newWidth)
+            .height(newWidth * $el.data('aspectRatio'));
+
+    });
+
+    // Kick off one resize to fix all videos on page load
+}).resize();
