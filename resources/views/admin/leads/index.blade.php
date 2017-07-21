62029b26855c2c889f986e4358752bba2c926ae0
Roberth Godoy <roberth.godoy@yahoo.com>
2017-07-21 20:39:48 +0000

    add progress bar

--- resources/views/admin/leads/index.blade.php
+++ resources/views/admin/leads/index.blade.php
@@ -3,9 +3,10 @@
 @section('content')
     
     <div>
+        <center><h4>{{ $leads['leads']->total() }} de 1000</h4></center><br/>
         <div class="progress">
-            <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="{{round($leads['leads']->total()*100/1000,0)}}" aria-valuemin="0" aria-valuemax="100" style="width: {{round($leads['leads']->total()*100/1000,0)}}%">
-                {{ round($leads['leads']->total()*100/1000,0) }}% alcançado | {{ $leads['leads']->total() }} / 1000
+            <div class="progress-bar progress-bar-success progress-bar-striped active" role="progressbar" aria-valuenow="{{round($leads['leads']->total()*100/1000,0)}}" aria-valuemin="0" aria-valuemax="100" style="width: {{round($leads['leads']->total()*100/1000,0)}}%;color: #244024">
+                {{ round($leads['leads']->total()*100/1000,0) }}% alcançado | 
             </div>
         </div>
     </div>
